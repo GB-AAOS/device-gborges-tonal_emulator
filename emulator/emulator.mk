cc1285005847bd0f9276a82c59a338ad7f608df7
Gabriel Borges <gabriel.borges@cariad.us>
2026-04-16 04:26:10 +0000

    include proximity cli in build

--- emulator/emulator.mk
+++ emulator/emulator.mk
@@ -9,7 +9,8 @@ PRODUCT_MODEL := Android SDK x86_64 emulator for Tonal Challenge
 # Tonal Challenge properties
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.multihal \
-    android.hardware.sensors@2.1-impl.gborges
+    android.hardware.sensors@2.1-impl.gborges \
+	ProximityCli
 
 PRODUCT_COPY_FILES += \
     device/gborges/emulator/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
