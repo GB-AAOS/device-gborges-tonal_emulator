dc777342b2d53dee1029fbebce9d1898397cccee
Gabriel Borges <gabriel.borges@cariad.us>
2026-04-16 04:25:31 +0000

    add conf file and build subHAL with device

--- emulator/emulator.mk
+++ emulator/emulator.mk
@@ -5,3 +5,11 @@ PRODUCT_NAME := emulator
 PRODUCT_DEVICE := emu64x
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := Android SDK x86_64 emulator for Tonal Challenge
+
+# Tonal Challenge properties
+PRODUCT_PACKAGES += \
+    android.hardware.sensors-service.multihal \
+    android.hardware.sensors@2.1-impl.gborges
+
+PRODUCT_COPY_FILES += \
+    device/gborges/emulator/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
