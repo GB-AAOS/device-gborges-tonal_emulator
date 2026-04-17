e620cb8ad41a02e9a3e0e93861b64eb061f41d80
Gabriel <gborges9@outlook.com>
2026-04-17 11:37:44 -0300

    rename emulator to tonal_emulator

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/emulator.mk 
+    $(LOCAL_DIR)/tonal_emulator.mk 
--- tonal_emulator.mk
+++ tonal_emulator.mk
@@ -1,18 +1,18 @@
 EMULATOR_VENDOR_NO_SENSORS := true
 PRODUCT_COPY_FILES += \
-    device/gborges/emulator/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+    device/gborges/tonal_emulator/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 
 $(call inherit-product, device/generic/goldfish/64bitonly/product/sdk_phone64_x86_64.mk)
 
 # Product properties
-PRODUCT_NAME := emulator
+PRODUCT_NAME := tonal_emulator
 PRODUCT_DEVICE := emu64x
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := Android SDK x86_64 emulator for Tonal Challenge
 
 DEVICE_MANIFEST_FILE := device/generic/goldfish/manifest.xml
-BOARD_VENDOR_SEPOLICY_DIRS += device/gborges/emulator/sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += device/gborges/tonal_emulator/sepolicy
 
 # Tonal Challenge properties
 PRODUCT_PACKAGES += \
