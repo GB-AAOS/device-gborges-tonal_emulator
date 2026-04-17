1f6a674bac0b097a3072e91c17ec9e39b3c1e9b2
Gabriel <gborges9@outlook.com>
2026-04-17 11:43:34 -0300

    log sensor value every second

--- apps/proximity_monitor_app/src/com/gborges/proximity_monitor/MainActivity.kt
+++ apps/proximity_monitor_app/src/com/gborges/proximity_monitor/MainActivity.kt
@@ -7,7 +7,10 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.os.SystemClock
+import android.util.Log
 import android.widget.TextView
 
 class MainActivity : Activity(), SensorEventListener {
@@ -18,6 +21,15 @@ class MainActivity : Activity(), SensorEventListener {
     private lateinit var nameText: TextView
     private lateinit var valueText: TextView
     private lateinit var latencyText: TextView
+
+    private var lastValue: Float = 0f
+    private val handler = Handler(Looper.getMainLooper())
+    private val logRunnable: Runnable = object : Runnable {
+        override fun run() {
+            Log.d("ProximityMonitor", "value: $lastValue")
+            handler.postDelayed(this, 1000)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,15 +54,18 @@ class MainActivity : Activity(), SensorEventListener {
         proximitySensor?.let {
             sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_FASTEST)
         }
+        handler.post(logRunnable)
     }
 
     override fun onPause() {
         super.onPause()
+        handler.removeCallbacks(logRunnable)
         sensorManager.unregisterListener(this)
     }
 
     override fun onSensorChanged(event: SensorEvent) {
         val value = event.values[0]
+        lastValue = value
         val timestampNs = event.timestamp
         val currentNs = SystemClock.elapsedRealtimeNanos()
         
